34efb54f7ac7a1d177da992a634bf6c79038bbed
LusineHarutyunyan <lusineharutyunyan07@gmail.com>
2020-12-10 22:52:32 +0400

    LessnonThreeExB

---
+++ src/com/company/LessnonThreeExB.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class LessnonThreeExB {
+    public static void main(String[] args) {
+        int N = 12;
+        int sum = 0;
+
+        for(int i = 1; i <= N; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
+
